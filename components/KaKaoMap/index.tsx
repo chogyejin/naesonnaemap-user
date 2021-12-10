7f3c7d7ea4d3e40d471232e6fe766106c66db7fa
chogyejin <whrpwls96@naver.com>
2021-12-10 18:01:50 +0900

    Feat: Add place list div sample' into Map component

--- components/KaKaoMap/index.tsx
+++ components/KaKaoMap/index.tsx
@@ -6,8 +6,21 @@ interface KakaomapComponentProps {
 const KakaomapComponent: React.FC<KakaomapComponentProps> = forwardRef(
   (props, ref) => {
     return (
-      <div style={{ width: '80vw', height: '80vh' }}>
+      <div style={{ width: '80vw', height: '80vh', position: 'relative' }}>
         <div ref={ref} style={{ width: '100%', height: '100%' }} />
+        <div
+          style={{
+            width: '20vw',
+            height: '40vh',
+            position: 'relative',
+            opacity: '0.7',
+            backgroundColor: 'white',
+            top: '-80vh',
+            zIndex: '1',
+            float: 'left',
+          }}>
+          places list div
+        </div>
       </div>
     );
   },
