beabd7edb129906f1af72912d593f003335e46e9
chogyejin <whrpwls96@naver.com>
2021-12-12 21:44:49 +0900

    Feat: add button(no click event) on places list and apply style in PlacesList component

--- components/KaKaoMap/PlacesList.tsx
+++ components/KaKaoMap/PlacesList.tsx
@@ -5,12 +5,15 @@ import { IPlace, placesState } from '../../recoil/states';
 export default function PlacesList() {
   const places = useRecoilValue<IPlace[]>(placesState);
 
+  function onAdd() {
+    alert('추가되었습니다.');
+  }
   return (
     <div>
       <div
         style={{
           width: '20vw',
-          height: '40vh',
+          height: '70vh',
           position: 'relative',
           opacity: '0.7',
           backgroundColor: 'white',
@@ -18,9 +21,32 @@ export default function PlacesList() {
           zIndex: '1',
           float: 'left',
         }}>
-        <div style={{ fontSize: '10px' }}>
+        <div style={{ borderBottom: 'solid 2px' }}>검색목록</div>
+        <div style={{ fontSize: '12px' }}>
           {places.map((place, key) => (
-            <div key={key}>{place.place_name}</div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                borderBottom: 'solid 1px',
+                height: '32px',
+              }}>
+              <div
+                style={{
+                  paddingBottom: '10px',
+                  paddingTop: '5px',
+                }}
+                key={key}>
+                {place.place_name.length > 10 ? (
+                  <div>{place.place_name.slice(0, 13)}...</div>
+                ) : (
+                  <div>{place.place_name}</div>
+                )}
+              </div>
+              <div>
+                <button onClick={onAdd}>버튼</button>
+              </div>
+            </div>
           ))}
         </div>
       </div>
