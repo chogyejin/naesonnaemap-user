c7801641d5904237a798d2085e7b8d0cd5579dbf
chogyejin <whrpwls96@naver.com>
2021-12-15 18:36:37 +0900

    Feat: add place delete button in mypage

--- pages/myPage.tsx
+++ pages/myPage.tsx
@@ -11,15 +11,19 @@ export default function MyPage({ myPlaces }: MyPageProps) {
 
   useEffect(() => {
     const { kakao } = window as any;
+    const infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
     const coords = new kakao.maps.LatLng(37.574515, 126.97693); //위도, 경도 순
     const options = {
       center: coords,
-      level: 9,
+      level: 10,
     };
     const map = new kakao.maps.Map(mapDiv.current, options);
 
+    kakao.maps.event.addListener(map, 'click', function () {
+      infowindow.close();
+    });
+
     function displayMarker(place: IPlace) {
-      const infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
       const marker = new kakao.maps.Marker({
         map: map,
         position: new kakao.maps.LatLng(place.y, place.x),
@@ -31,7 +35,6 @@ export default function MyPage({ myPlaces }: MyPageProps) {
           `<div style="padding:5px;font-size:12px;">
             <div style="display:flex;">
               <div>${place.place_name}</div>
-              <button>추가</button>
             </div>
             <div>
               <div>
@@ -43,6 +46,11 @@ export default function MyPage({ myPlaces }: MyPageProps) {
               <div>
                 ${place.phone}
               </div>
+              <div>
+                <a href=${place.place_url} target=_blank>
+                홈페이지
+                </a>
+              </div>
             </div>
           </div>`,
         );
@@ -53,19 +61,46 @@ export default function MyPage({ myPlaces }: MyPageProps) {
     myPlaces.map((place) => displayMarker(place));
   }, [mapDiv]);
 
+  async function onDelete(placeId: string) {
+    const isDelete = confirm('삭제하시겠습니까?');
+    if (isDelete) {
+      const result = await axios.delete(
+        `http://localhost:4000/places/${placeId}`,
+      );
+      if (result.status === 200) {
+        alert('삭제되었습니다');
+      }
+    }
+  }
+
   return (
     <div>
-      <div>
-        <h1>내 찜 리스트</h1>
-      </div>
-      {myPlaces.map((place, key) => (
-        <div key={key}>
-          <div>{place.place_name}</div>
-          <div>{place.address_name}</div>
+      <div style={{ display: 'flex' }}>
+        <div style={{ width: '60vw', height: '80vh' }}>
+          <div ref={mapDiv} style={{ width: '100%', height: '100%' }}></div>
         </div>
-      ))}
-      <div style={{ width: '80vw', height: '80vh' }}>
-        <div ref={mapDiv} style={{ width: '100%', height: '100%' }}></div>
+        <div>
+          <div>
+            <h1>내 찜 리스트</h1>
+          </div>
+          {myPlaces.map((place, key) => (
+            <div
+              key={key}
+              style={{
+                marginBottom: '20px',
+                display: 'flex',
+                justifyContent: 'space-between',
+              }}>
+              <div style={{ textAlign: 'left' }}>
+                <div>{place.place_name}</div>
+                <div>{place.address_name}</div>
+              </div>
+              <div>
+                <button onClick={() => onDelete(place.id)}>삭제</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
